5b20c5390d129e5760d9a41d1d960b474dbcd744
Nishi Sharma <84862478+Nishi-Sharma24@users.noreply.github.com>
2022-04-02 23:32:20 +0530

    Add files via upload

---
+++ month and days.py
@@ -0,0 +1,27 @@
+# Write a program to accept a number from 1 to 12 and display name of the month and days in that month like 1 for January and number of days 31 and so on.
+n=int(input("Enter any number between 1 to 12 : "))
+if n==1:
+    print("The month is January and it has 31 days.")
+elif n==2:
+    print(" The month is February and it has 28/29 days.")
+elif n==3:
+    print(" The month is March and it has 31 days.")
+elif n==4:
+    print(" The month is April and it has 30 days.")
+elif n==5:
+    print(" The month is May and it has 31 days.")
+elif n==6:
+    print(" The month is June and it has 30 days.")
+elif n==7:
+    print("The month is July and it has 31 days.")
+elif n==8:
+    print("The month is August and it has 31 days.")
+elif n==9:
+    print("The month is September and it has 30 days.")
+elif n==10:
+    print("The month is October and it has 31 days.")
+elif n==11:
+    print("The month is November and it has 30 days.")
+elif n==12:
+    print("The month is December and it has 31 days.")
+input()
